c3ccbd51e6dc1b86ecb09fee8c872fd7b27d9641
danyowoj <latsuk1803@gmail.com>
2024-11-06 12:43:55 +0700

    Add comments to fibonacci_nums.pl

--- lab_5/fibonacci_nums.pl
+++ lab_5/fibonacci_nums.pl
@@ -1,5 +1,10 @@
-fib(0, 1).  % the basic case: F(0) = 1
-fib(1, 1).  % the basic case: F(1) = 1
+% basic cases F(0) = F(1) = 1
+fib(0, 1).
+fib(1, 1).
+
+% a recursive function that calculates the Nth Fibonacci number.
+% It checks whether N is greater than 1. If so, recursively calls itself for N-1 and N-2 
+% and then summarizes the results.
 fib(N, F) :-
     N > 1,
     N1 is N - 1,
@@ -10,11 +15,17 @@ fib(N, F) :-
 
 
 start :-
+    % this is the main loop that uses repeat to repeat the query multiple times
     repeat,
         write('Enter the number of the Fibonacci number (a negative number to exit): '),
         read(N),
-        (   N < 0 -> !, fail  % Условие выхода из цикла: отрицательное число
-        ;   fib(N, F),
-            writef('Число Фибоначчи F(%t) = %t\n', [N, F]),
-            fail % Имитация цикла с помощью fail и repeat
+        % repeat by itself does not control the exit of the loop
+        % this is done using the loop exit condition
+        % if the entered number N is less than 0, then ! (cut) cuts off the search 
+        % for alternative solutions, and fail causes repeat to end
+        (   N < 0 -> !, fail
+        % if N >= 0 than the following part is executed
+        ;   fib(N, F), % call function fib to calculate the Fibonacci number
+            writef('Число Фибоначчи F(%t) = %t\n', [N, F]), % displays the result
+            fail % loop simulation using fail and repeat
         ).
